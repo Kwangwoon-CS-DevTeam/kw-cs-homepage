c01881affe4d115d178eb6ef094767d52f2c1f30
holychan <east0321@outlook.kr>
2024-12-20 23:19:27 +0900

    Init: TailwindCSS 초기화 및 초기설정

---
+++ front/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // React 파일 경로
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
