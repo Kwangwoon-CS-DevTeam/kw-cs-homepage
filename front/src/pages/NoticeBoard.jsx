3d1cb62463766def19d3c210809fb53af04faf84
holychan <east0321@outlook.kr>
2024-12-25 03:44:15 +0900

    Feat: 공지사항 h1 태그 폰트 크기 변경

--- front/src/pages/NoticeBoard.jsx
+++ front/src/pages/NoticeBoard.jsx
@@ -11,7 +11,7 @@ function NoticeBoard() {
                 className="bg-white pt-24 sm:pt-32 lg:pt-48 pb-16 sm:pb-24 lg:pb-28 px-4 sm:px-8 lg:px-16"> {/* 반응형 여백 */}
                 <div className="container mx-auto">
                     {/* 제목 */}
-                    <h1 className="text-4xl font-extrabold text-neutral-800 text-left"> {/* 왼쪽 정렬 */}
+                    <h1 className="text-4xl font-bold text-neutral-800 text-left"> {/* 왼쪽 정렬 */}
                         공지사항
                     </h1>
                     {/* 서브텍스트 */}
