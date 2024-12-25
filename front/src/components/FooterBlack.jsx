f0759d88a56f8a59f76a5ed2c998cebfa3e37b07
holychan <east0321@outlook.kr>
2024-12-25 11:36:44 +0900

    Design: Footer 블랙 버전 색상 수정

--- front/src/components/FooterBlack.jsx
+++ front/src/components/FooterBlack.jsx
@@ -8,21 +8,16 @@ export default function Footer() {
             <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
                 {/* 왼쪽 영역 */}
                 <div className="text-center md:text-left">
-                    <h2 className="text-xl font-bold bg-blue-800 mb-2 p-1 rounded">
+                    <h2 className="text-xl font-bold text-blue-950 mb-2 p-1 rounded">
                         {footer.left.title}
                     </h2>
-                    <p className="text-sm">{footer.left.president}</p>
-                    <p className="text-sm">{footer.left.vicePresident}</p>
-                </div>
-
-                {/* 중앙 영역 */}
-                <div className="text-center mt-4 md:mt-0">
-                    <p className="text-sm">{footer.center.message}</p>
+                    <p className="text-sm text-blue-950">{footer.left.president}</p>
+                    <p className="text-sm text-blue-950">{footer.left.vicePresident}</p>
                 </div>
 
                 {/* 오른쪽 영역 */}
                 <div className="text-center md:text-right mt-4 md:mt-0">
-                    <p className="text-sm font-semibold">{footer.right.contact}</p>
+                    <p className="text-sm font-semibold text-blue-950">{footer.right.contact}</p>
                     <div className="flex justify-center md:justify-end mt-2">
                         <img
                             src={footer.right.logoSrcBlack}
