3590b7b233e872fe1d4602d2d20a9516b3fcb5fa
holychan <east0321@outlook.kr>
2024-12-26 11:42:20 +0900

    Feat: 라우트 연결

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NoticeBoard from './pages/NoticeBoard';
 import ResourceRoom from './pages/ResourceRoom';
+import NotFoundPage from './pages/404';
 
 function App() {
   return (
@@ -16,6 +17,17 @@ function App() {
                 {/* 자료실 페이지 */}
                 <Route path="/resources" element={<ResourceRoom />} />
 
+                {/* 세부전공 페이지 */}
+                <Route path="/specializations" element={<NotFoundPage />} />
+
+                {/* QnA 페이지 */}
+                <Route path="/qna" element={<NotFoundPage />} />
+
+                {/* 로그인 페이지 */}
+                <Route path="/login" element={<NotFoundPage />} />
+
+                {/* 404 페이지 */}
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
       </Router>
   );
