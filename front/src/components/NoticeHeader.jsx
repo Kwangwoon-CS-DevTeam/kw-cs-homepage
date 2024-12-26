cb157d8b5bd9217744a266ca3534a8cc5be79ee6
holychan <east0321@outlook.kr>
2024-12-27 03:22:51 +0900

    Design: Navbar relative 로 수정한 여파 복구

--- front/src/components/NoticeHeader.jsx
+++ front/src/components/NoticeHeader.jsx
@@ -1,15 +1,18 @@
 import PropTypes from "prop-types"; // PropTypes 임포트
 
-export default function NoticeHeader({title, sub}) {
-    return(
+export default function NoticeHeader({ title, sub }) {
+    return (
         <div>
             {/* 공지사항 헤더 */}
-            <div className="bg-white pt-24 sm:pt-32 lg:pt-48 pb-16 sm:pb-24 lg:pb-28 px-4 sm:px-8 lg:px-16">
+            <div
+                className="bg-white pt-28 sm:pt-32 lg:pt-36 pb-12 sm:pb-20 lg:pb-24 px-4 sm:px-8 lg:px-16"
+                style={{ marginTop: "-4rem" }} // Navbar 크기만큼 조정
+            >
                 <div className="container mx-auto">
-                    <h1 className="text-4xl font-bold text-neutral-800 text-left">{title}</h1>
-                    <p className="text-lg text-blue-950 mt-4 text-left">
-                        {sub}
-                    </p>
+                    <h1 className="text-4xl font-bold text-neutral-800 text-left">
+                        {title}
+                    </h1>
+                    <p className="text-lg text-blue-950 mt-4 text-left">{sub}</p>
                 </div>
             </div>
 
@@ -18,11 +21,11 @@ export default function NoticeHeader({title, sub}) {
                 <hr className="border-t border-gray-300" />
             </div>
         </div>
-    )
+    );
 }
 
 // PropTypes 정의
 NoticeHeader.propTypes = {
     title: PropTypes.string.isRequired,
-    sub: PropTypes.string.isRequired
+    sub: PropTypes.string.isRequired,
 };
