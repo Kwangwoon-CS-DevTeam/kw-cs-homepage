990458eaae39f0899213397c6dcb46ae448e2a14
holychan <east0321@outlook.kr>
2025-01-09 15:55:22 +0900

    Chore: supabase DB 를 sequelize 를 통해서 연결

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,24 @@
+require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db'); // db.js에서 가져오기
+
+// 모델 동기화
+(async () => {
+    try {
+        await sequelize.sync();
+        console.log('모델 동기화 성공!');
+    } catch (error) {
+        console.error('모델 동기화 실패:', error);
+    }
+})();
+
 
 const app = express();
-
-const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.send('CI CD 테스트 완료, devbranch 로 변경합니다.');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+// 환경 변수 확인
+if (!process.env.DB_URL) {
+    throw new Error('DATABASE_URL 환경 변수가 설정되지 않았습니다.');
+}
+
+// Sequelize 초기화
+const sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+    logging: true, // 로깅 비활성화
+});
+
+module.exports = sequelize;
