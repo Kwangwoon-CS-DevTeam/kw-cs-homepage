3f56d140cf85d858419a5bb30811bbc1a873dff9
holychan <east0321@outlook.kr>
2025-01-09 23:39:48 +0900

    Chore: for swagger test

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,19 @@ const app = express();
 // Swagger UI 세팅
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Welcome 메시지 반환
+ *     responses:
+ *       200:
+ *         description: 성공적으로 메시지를 반환함
+ */
+app.get('/', (req, res) => {
+    res.send('Welcome to the API!');
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
