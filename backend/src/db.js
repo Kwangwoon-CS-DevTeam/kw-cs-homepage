4e6e9e8ca0bcb9e54d8c9ed86cf3da6d641ca38a
holychan <east0321@outlook.kr>
2025-01-10 12:56:00 +0900

    Feat: Sequelize 설정 파일에서 DB 연결 테스트 코드 추가

--- backend/src/db.js
+++ backend/src/db.js
@@ -12,4 +12,8 @@ const sequelize = new Sequelize(process.env.DB_URL, {
     logging: false, // 로깅 비활성화
 });
 
+sequelize.authenticate()
+    .then(() => console.log('데이터베이스 연결 성공'))
+    .catch((err) => console.error('데이터베이스 연결 실패:', err));
+
 module.exports = sequelize;
