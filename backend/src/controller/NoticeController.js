1d744feeb56190e484fcef1f0328b465ededa85f
holychan <east0321@outlook.kr>
2025-01-11 22:46:56 +0900

    Feat: 공지사항 저장 컨트롤러 로직 작성

--- backend/src/controller/NoticeController.js
+++ backend/src/controller/NoticeController.js
@@ -9,3 +9,26 @@ exports.getPaginatedNotices = async (req, res) => {
         res.status(500).json({ error: 'Internal Server Error' });
     }
 };
+
+exports.createNotice = async (req, res) => {
+    const noticeData = req.body; // 클라이언트에서 보낸 데이터
+
+    try {
+        const result = await noticeService.saveNotice(noticeData);
+
+        if (result.success) {
+            res.status(201).json({
+                message: '공지사항이 성공적으로 저장되었습니다.',
+                data: result.data,
+            });
+        } else {
+            res.status(400).json({
+                message: result.message,
+                error: result.error,
+            });
+        }
+    } catch (error) {
+        console.error('공지사항 저장에 실패하였습니다 in Controller: ', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
