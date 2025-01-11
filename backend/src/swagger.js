767137382af3fd21641247cc910ceb3a7e715fbb
holychan <east0321@outlook.kr>
2025-01-11 23:53:02 +0900

    Chroe: Notice 스키마 작성

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -13,8 +13,63 @@ const swaggerOptions = {
                 url: 'http://localhost:3000', // 서버 URL
             },
         ],
+        components: {
+            schemas: {
+                Notice: {
+                    type: 'object',
+                    properties: {
+                        id: {
+                            type: 'integer',
+                            example: 1,
+                        },
+                        admin_id: {
+                            type: 'string',
+                            example: 'admin123',
+                        },
+                        category_id: {
+                            type: 'integer',
+                            example: 1,
+                        },
+                        title: {
+                            type: 'string',
+                            example: '공지사항 제목',
+                        },
+                        content: {
+                            type: 'string',
+                            example: '<p>공지 내용입니다.</p>',
+                        },
+                        url: {
+                            type: 'string',
+                            example: 'http://example.com',
+                        },
+                        max_participants: {
+                            type: 'integer',
+                            example: 100,
+                        },
+                        current_participants: {
+                            type: 'integer',
+                            example: 50,
+                        },
+                        created_at: {
+                            type: 'string',
+                            format: 'date-time',
+                            example: '2025-01-11T12:34:56.789Z',
+                        },
+                        updated_at: {
+                            type: 'string',
+                            format: 'date-time',
+                            example: '2025-01-11T12:34:56.789Z',
+                        },
+                        isDeleted: {
+                            type: 'integer',
+                            example: 0,
+                        },
+                    },
+                },
+            },
+        },
     },
-    apis: ['./src/*.js'], // 여러 경로 추가
+    apis: ['./src/controllers/*.js'], // 여러 경로 추가
 };
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
