09f50e0d86846525b1204fc75ba74c117520219a
holychan <east0321@outlook.kr>
2025-01-13 18:59:31 +0900

    ci: amplify 배포

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 빌드 결과물이 저장될 폴더
+  },
   css: {
     postcss: './postcss.config.js', // PostCSS 설정 경로
   },
