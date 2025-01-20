010b63ae23f418fca10792e0feb6b111b84b2406
dragonjjin <dydwls0868@naver.com>
2025-01-20 17:19:15 +0900

    no message

--- backend/src/middlewares/validationMiddleware.js
+++ backend/src/middlewares/validationMiddleware.js
@@ -1,14 +1,13 @@
 const { validationResult } = require('express-validator');
 
-const handleValidationErrors = (req, res, next) => {
+const validateRequest = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({
-            message: 'Validation Error',
-            errors: errors.array().map((error) => error.msg),
+            errors: errors.array(),
         });
     }
     next();
 };
 
-module.exports = { handleValidationErrors };
+module.exports = validateRequest;
