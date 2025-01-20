fdc948c0730125337379a2800d0b07e2b422bad9
dragonjjin <dydwls0868@naver.com>
2025-01-20 18:10:15 +0900

    Feat : Auth 미들웨어 추가

--- backend/src/routes/QuestionsRoutes.js
+++ backend/src/routes/QuestionsRoutes.js
@@ -1,23 +1,24 @@
 // routes/questions.js
 const express = require('express');
 const router = express.Router();
+const verifyAuth = require('../middlewares/authMiddleware');
 const questionsController = require('../controllers/questionsController'); // 컨트롤러 가져오기
-const validateRequest = require("../middlewares/validationMiddleware"); // 유효성 검사 미들웨어
+const validationMiddleware = require('../middlewares/validationMiddleware');
 const { 
     createQuestionValidator,
     updateAnswerValidator,
     deleteQuestionValidator } = require('../validators/QuestionsValidator');
 
 // 1. 질문 등록 (Create)
-router.post('/add', createQuestionValidator, validateRequest, questionsController.createQuestion);
+router.post('/add', createQuestionValidator, validationMiddleware.handleValidationErrors, questionsController.createQuestion);
 
 // 2. 질문 목록 조회 (Read)
-router.get('/', updateAnswerValidator, validateRequest, questionsController.getQuestions);
+router.get('/', questionsController.getQuestions);
 
 // 3. 질문 답변 작성 (Update)
-router.put('/answer/:id', questionsController.updateAnswer);
+router.put('/answer/:id', verifyAuth, updateAnswerValidator, validationMiddleware.handleValidationErrors, questionsController.updateAnswer);
 
 // 4. 질문 삭제 (Delete) - 소프트 삭제
-router.delete('/delete/:id', deleteQuestionValidator, validateRequest, questionsController.deleteQuestion);
+router.delete('/delete/:id', verifyAuth, deleteQuestionValidator, validationMiddleware.handleValidationErrors, questionsController.deleteQuestion);
 
 module.exports = router;
