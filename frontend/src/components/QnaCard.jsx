8611c38d775f2faccc519e63bdae368045e22677
holychan <east0321@outlook.kr>
2025-01-24 20:18:36 +0900

    Design: Q&A 카드 모바일 맞춤형 디자인 개선

--- frontend/src/components/QnaCard.jsx
+++ frontend/src/components/QnaCard.jsx
@@ -7,30 +7,31 @@ const QnACard = ({ data }) => {
         <div className="w-full max-w-screen-lg mx-auto px-4">
             <Link
                 to={`/qna/${id}`}
-                className="flex items-center justify-between
-                     w-full
-                     px-3 sm:px-4 lg:px-6
-                     py-5 sm:py-5 lg:py-6
-                     border rounded-lg shadow-sm
-                     cursor-pointer hover:shadow-md transition-shadow duration-300"
+                className="flex flex-col sm:flex-row items-start sm:items-center justify-between
+                 w-full
+                 px-3 sm:px-4 lg:px-6
+                 py-2.5 sm:py-5 lg:py-6
+                 border rounded-lg shadow-sm
+                 cursor-pointer hover:shadow-md transition-shadow duration-300"
             >
                 {/* 왼쪽 그룹 */}
-                <div className="flex flex-row space-x-2 sm:space-x-4 lg:space-x-6">
+                <div className="flex flex-row space-x-2 sm:space-x-4 lg:space-x-6 mb-2 sm:mb-0">
                     <span className="text-gray-500 text-sm lg:text-base font-light">#{id}</span>
                     <span className="text-black text-sm lg:text-base font-semibold">
-                        {title || "제목 없음"}
-                    </span>
+                {title || "제목 없음"}
+            </span>
                 </div>
 
                 {/* 오른쪽 그룹 */}
-                <div className="flex flex-row items-end space-x-2 sm:space-x-4 lg:space-x-6">
-                    <span className="text-gray-500 text-sm lg:text-base font-light">{nickname}</span>
-                    <span className="text-gray-500 text-sm lg:text-base font-light">
-                        {new Date(created_at).toLocaleDateString()}
+                <div
+                    className="flex flex-row sm:flex-row sm:items-end sm:justify-end sm:space-x-2 space-x-4 lg:space-x-6 w-full sm:w-auto text-left sm:text-right">
+                    <span className="text-gray-500 text-xs lg:text-base font-light mt-1.5 sm:mt-0 sm:mb-1">{nickname}</span>
+                    <span className="text-gray-500 text-xs lg:text-base font-light mt-1.5 sm:mt-0 sm:mb-1">
+                    {new Date(created_at).toLocaleDateString()}
                     </span>
                     <span
-                        className={`px-3 py-1 rounded text-white text-center 
-                        text-sm lg:text-base ${
+                        className={`px-2 py-1 rounded text-white text-center
+                            text-xs lg:text-base ${
                             admin_id ? "bg-blue-800" : "bg-gray-500"
                         }`}
                     >
