7102c7e83fa313c5d8ca711d36ca102ad448743c
holychan <east0321@outlook.kr>
2025-01-26 09:21:25 +0900

    Design: Q&A 상세페이지 모바일 맞춤형 디자인 개선

--- frontend/src/pages/QnaDetailPage.jsx
+++ frontend/src/pages/QnaDetailPage.jsx
@@ -129,36 +129,39 @@ const QnaDetailPage = () => {
     return (
         <div>
             <NavbarBlack />
-            <div className="container mx-auto mt-10 px-8">
-                <h1 className="text-3xl font-bold mb-6">Q&A</h1>
+            <div className="container mx-auto mt-10 sm:px-8">
+                <h1 className="text-3xl font-bold mb-6 px-8">Q&A</h1>
 
                 {/* 질문 상세 내용 */}
-                <div className="bg-white shadow-[0_0_6px_rgba(0,0,0,0.25)] rounded-lg p-6 mb-12 px-8">
+                <div className="bg-white sm:shadow-[0_0_6px_rgba(0,0,0,0.25)] sm:rounded-lg mb-12 sm:p-6 sm:mb-12 sm:px-8">
                     <div className="px-7 mt-8 mb-10">
-                        <div className="flex justify-between items-center">
+                        <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
                             {isEditing ? (
                                 <input
                                     type="text"
                                     value={editableQuestion.title}
-                                    onChange={(e) => setEditableQuestion({ ...editableQuestion, title: e.target.value })}
-                                    className="text-2xl font-semibold w-3/4 px-3 py-2 border-neutral-100 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-400"
+                                    onChange={(e) => setEditableQuestion({...editableQuestion, title: e.target.value})}
+                                    className="text-2xl font-semibold w-full sm:w-3/4 px-3 py-2 border-neutral-100 border rounded-md focus:outline-none focus:ring-1 focus:ring-gray-400"
                                 />
                             ) : (
-                                <h2 className="text-2xl font-semibold">{question.title || "제목 없음"}</h2>
+                                <h2 className="text-2xl font-semibold text-left">{question.title || "제목 없음"}</h2>
                             )}
-                            <div className="flex space-x-4 text-gray-500 text-sm">
+                            <div className="flex items-center space-x-4 text-gray-500 text-sm mt-2">
                                 <span>{question.nickname}</span>
                                 <span>{new Date(question.created_at).toLocaleDateString()}</span>
                             </div>
                         </div>
 
-                        <hr className="border-t border-[1px] border-neutral-500 my-2 mb-10" />
+                        <hr className="border-t border-[1px] border-neutral-500 my-2 mb-10"/>
 
                         <div>
                             {isEditing ? (
                                 <textarea
                                     value={editableQuestion.question}
-                                    onChange={(e) => setEditableQuestion({ ...editableQuestion, question: e.target.value })}
+                                    onChange={(e) => setEditableQuestion({
+                                        ...editableQuestion,
+                                        question: e.target.value
+                                    })}
                                     className="w-full px-3 py-2 border border-neutral-100 border rounded-md h-40 focus:outline-none focus:ring-1 focus:ring-gray-400"
                                 />
                             ) : (
@@ -171,24 +174,24 @@ const QnaDetailPage = () => {
                 {/* 조건부 렌더링 */}
                 {question.admin_id ? (
                     // 답변 작성 내용 (관리자가 작성한 답변이 있는 경우)
-                    <div className="bg-neutral-100 border-neutral-200 border-[1px] rounded-lg p-6 mb-6">
-                        <div className="px-7 mt-10 mb-10">
-                            <div className="flex justify-between items-center">
-                                <h2 className="text-2xl font-bold mb-4">질문하신 글에 대한 답변입니다.</h2>
-                                <div className="flex space-x-4 text-gray-500 text-sm">
+                    <div className="bg-neutral-100 border-neutral-200 border-[1px] rounded-lg p-6 pt-2 mb-6">
+                        <div className="sm:px-7 mt-10 mb-10">
+                            <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center">
+                                <h2 className="text-2xl font-bold sm:mb-2">질문하신 글에 대한 답변입니다.</h2>
+                                <div className="flex items-center space-x-4 text-gray-500 text-sm mt-2 sm:mt-0">
                                     <span>관리자: {question.admin_id}</span>
                                     <span>{new Date(question.updated_at).toLocaleDateString()}</span>
                                 </div>
                             </div>
                             {/* 구분선 */}
-                            <hr className="border-t border-[1px] border-neutral-200 my-2 mb-10" />
+                            <hr className="border-t border-[1px] border-neutral-200 my-2 mb-10"/>
                             <p className="text-black">{question.answer}</p>
                         </div>
                     </div>
                 ) : (
                     // 답변이 없는 경우
                     <>
-                        {isEditing ? (
+                    {isEditing ? (
                             // 수정 모드: 취소 및 저장 버튼 표시
                             <div className="flex justify-end space-x-4 mt-4 mb-56">
                                 <button
